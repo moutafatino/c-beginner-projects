af3e82f0d1ca121c7093d388320656971c1971ba
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-05 13:24:56 +0100

    get a random number

--- number-guessing-game/src/main.c
+++ number-guessing-game/src/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) { return 0; }
+#define RANDOM_NUMBER_RANGE (11)
+
+int main(void) {
+
+  long int random_number = arc4random_uniform(RANDOM_NUMBER_RANGE);
+
+  printf("%ld", random_number);
+  return 0;
+}
