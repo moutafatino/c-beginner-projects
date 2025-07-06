e2c0a9fa585fce497b2aa3c492f53bc8b74311ee
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-06 21:46:50 +0100

    create todo struct and display help menu

--- src/main.c
+++ src/main.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) { return 0; }
+struct Todo {
+  int ID;
+  char *text;
+};
+
+void show_help_menu(void) {
+  printf("--- Todo List Menu ---\n");
+
+  printf("1. Add Todo\n");
+  printf("2. View Todos\n");
+  printf("3. Help\n");
+  printf("0. Exit\n");
+}
+
+int main(void) {
+
+  show_help_menu();
+
+  return 0;
+}
