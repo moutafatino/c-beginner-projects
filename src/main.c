9b63ccb8afb9b81a8845eccbf635a51c1b47914d
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-06 22:05:38 +0100

    get user choice and make sure its valid

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Todo {
   int ID;
@@ -15,9 +17,37 @@ void show_help_menu(void) {
   printf("0. Exit\n");
 }
 
+int get_user_choice(char *prompt) {
+  char buffer[8];
+  char *endptr;
+  long number = -1;
+
+  while (true) {
+    printf("%s", prompt);
+    if (fgets(buffer, 8, stdin)) {
+      buffer[strcspn(buffer, "\n")] = 0;
+
+      number = strtol(buffer, &endptr, 10);
+
+      if (endptr == buffer || (*endptr && *endptr != '\n')) {
+        fprintf(stderr, "Invalid input: Please enter a valid number.\n");
+        continue;
+      }
+
+      break;
+    }
+  }
+
+  return (int)number;
+}
+
 int main(void) {
 
   show_help_menu();
+
+  int choice = get_user_choice("Enter your choice: ");
+
+  printf("User choice: %d", choice);
 
   return 0;
 }
