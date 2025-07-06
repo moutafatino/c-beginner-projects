86c7faa9001d2893746c9de9c216ee56b824dacd
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-06 14:34:01 +0100

    setup new project: console based todo list

--- src/main.c
+++ src/main.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-  int *age = malloc(sizeof(int));
-  const int AGE = 10;
-  *age = AGE;
-  printf("Hello, World! %d\n", *age);
-  return 0;
-}
+int main(void) { return 0; }
