0c6b53ebbb705093842ff9ce85c313b51678a20e
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-06 11:11:19 +0100

    add a little documentation for the util function

--- number-guessing-game/include/utils.h
+++ number-guessing-game/include/utils.h
@@ -1,5 +1,13 @@
 #ifndef _UTILS_H
 #define _UTILS_H
+/**
+ * @brief Get an integer value from the user within a specified range.
+ * @param min The minimum valid value (inclusive).
+ * @param max The maximum valid value (inclusive).
+ * @param format The format string for the prompt.
+ * @param ... Additional arguments for the format string.
+ *
+ */
 int get_int_value(int min, int max, const char *format, ...);
 
 #endif
