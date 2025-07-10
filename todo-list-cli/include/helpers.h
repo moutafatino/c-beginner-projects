4e1b4ad95c66403de0e0df190e6ff2ae966f176f
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-10 13:23:38 +0100

    add documentation to headerfiles

--- todo-list-cli/include/helpers.h
+++ todo-list-cli/include/helpers.h
@@ -10,9 +10,35 @@ enum input_error {
   INPUT_ERROR_INVALID_PARAM
 };
 
+/*
+ * @brief Returns a string representation of the input_error enum.
+ * @param error The input error enum value.
+ * @return A string describing the error.
+ * */
 const char *input_error_string(enum input_error error);
 
+/*
+ * @brief Prompts the user for an integer input and returns the result and keeps
+ * prompting until a valid integer is entered.
+ * @param prompt The prompt to display to the user.
+ * @return The integer input from the user.
+ * */
 int get_int_input(char *prompt);
+
+/*
+ * @brief Prompts the user for a choice from a list of valid options and keeps
+ * prompting until a valid choice is made.
+ * @param prompt The prompt to display to the user.
+ * @param valid_choices An array of valid choices.
+ * @param size The number of valid choices in the array.
+ * @return The user's choice as an integer.
+ * */
 int get_user_choice(char *prompt, int valid_choices[], size_t size);
 
+/*
+ * @brief Prompts the user for input and returns the result.
+ * @param prompt The prompt to display to the user.
+ * @param result Pointer to a char pointer where the result will be stored.
+ * @return An enum indicating the success or failure of the input operation.
+ * */
 enum input_error get_user_input(char *prompt, char **result);
--- todo-list-cli/include/todos.h
+++ todo-list-cli/include/todos.h
@@ -10,25 +10,65 @@ enum create_todo_result {
 };
 
 struct Todo {
-  int ID;
-  char *text;
-  bool is_done;
-};
-struct App {
-  struct Todo *items;
-  size_t length;
-  size_t capacity;
-  int next_id;
+  int ID;       // Unique identifier for the todo item
+  char *text;   // The text of the todo item
+  bool is_done; // Status of the todo item, true if done, false if not
 };
 
+/*
+ * @brief Represents the application state.
+ * */
+struct App {
+  struct Todo *items; // Array of todo items
+  size_t length;      // Size of the todo items array
+  size_t capacity;    // Capacity of the allocated memory for todo items
+  int next_id;        // Next ID to assign to a new todo item
+};
+
+/*
+ * @brief Initializes the application and loads todo items from a file.
+ * @return A pointer to the initialized App structure.
+ * */
 struct App *init_app(void);
 
+/*
+ * @brief Saves the todo items to a file.
+ * @param app The application instance containing the todo items.
+ * */
 void save_todos(struct App *app);
 
+/*
+ * @brief Lists all todo items in the application.
+ * @param todos The application instance containing the todo items.
+ * */
 void list_todos(struct App *todos);
+
+/*
+ * @brief Toggles the status of a todo item, Sets it to done if not done, and
+ * vice versa.
+ * @param app The application instance containing the todo items.
+ * @id The ID of the todo item to toggle.
+ * @return A message indicating the new status of the todo item or NULL if the
+ * ID is invalid.
+ * */
 const char *toggle_todo_status(struct App *app, int id);
 
+/*
+ * @brief Creates a new todo item.
+ * @param app The application instance to add the todo item to.
+ * @return The result of the creation operation.
+ * */
 enum create_todo_result create_new_todo(struct App *app);
 
+/*
+ * @brief Cleans up the todo items.
+ * @param items The array of todo items to free.
+ * @param size The number of items in the array.
+ * */
 void free_todos(struct Todo *items, size_t size);
+
+/*
+ * @brief Cleans up the application resources.
+ * @param app The application instance to clean up.
+ * */
 void end_app(struct App *app);
